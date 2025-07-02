d009fbb946b894dcaeaefad9cd7ffe52a157ad57
SRparadox <shazer.rizzo@gmail.com>
2025-07-01 22:04:57 -0700

    updating to nes urld

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         port: 3000,
     },
     plugins: [react()],
-    base: "/",
+    base: "/Updated-VTM-5-Creator/",
     resolve: {
         alias: {
             "~": path.resolve(__dirname, "src"),
