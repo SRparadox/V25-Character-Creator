f3f230de3b761d0c55e157d2bca0ba44c1612dbb
SRparadox <shazer.rizzo@gmail.com>
2025-10-25 18:12:58 -0700

    Add Vicissitude and Valeren disciplines, update vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         port: 3000,
     },
     plugins: [react()],
-    base: "/v25/",
+    base: "/V25-Character-Creator/",
     resolve: {
         alias: {
             "~": path.resolve(__dirname, "src"),
